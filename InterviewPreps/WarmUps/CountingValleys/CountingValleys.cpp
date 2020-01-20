d5d382690249af00ce3fc21d25b27f87ae9efb2c
Ahmed Karaibrahimov <frozen_heart88@abv.bg>
2020-01-20 10:09:22 +0200

    Solved Counting Values

--- InterviewPreps/WarmUps/CountingValleys/CountingValleys.cpp
+++ InterviewPreps/WarmUps/CountingValleys/CountingValleys.cpp
@@ -1,20 +1,49 @@
-// CountingValleys.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+constexpr const char upStep = 'U';
+constexpr const char downStep = 'D';
+
+// Complete the countingValleys function below.
+int countingValleys(int n, string s) 
+{
+    // Initially at the sea level
+    int depth = 0;
+    int valeysCount = 0;
+
+    for (auto stepChar : s)
+    {
+        if (stepChar == downStep)
+        {
+            --depth;
+        }
+        else
+        {
+            ++depth;
+            // If the hiker is at the sea level, 
+            // he/she has passed one valey
+            if (depth == 0)
+            {
+                ++valeysCount;
+            }
+        }
+    }
+    return valeysCount;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string s;
+    getline(cin, s);
+
+    int result = countingValleys(n, s);
+
+    cout << result << "\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
